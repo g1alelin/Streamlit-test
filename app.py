d5f9186210a69b9ac779273fba01d167d4198d66
Alexander Lindahl <alexander.lindahl@grundskola.goteborg.se>
2026-06-03 12:03:05 +0200

    metabase modified

--- app.py
+++ app.py
@@ -895,18 +895,17 @@ if tab_map:
                 <script defer src="http://metabase-609ip-gsf-analyslager.apps.k8sp.gbgpaas.se/app/embed.js"></script>
                 <script>
                 function defineMetabaseConfig(config) {
-                window.metabaseConfig = config;
+                  window.metabaseConfig = config;
                 }
                 </script>
 
                 <script>
-                defineMetabaseConfig({
+                  defineMetabaseConfig({
                     "isGuest": true,
                     "instanceUrl": "http://metabase-609ip-gsf-analyslager.apps.k8sp.gbgpaas.se"
-                });
+                  });
                 </script>
-
-                <metabase-dashboard with-title="true" with-downloads="false"></metabase-dashboard>
+                <metabase-dashboard token="eyJhbGciOiJIUzI1NiJ9.eyJyZXNvdXJjZSI6eyJkYXNoYm9hcmQiOjExfSwicGFyYW1zIjp7fSwiaWF0IjoxNzgwNDgwNzg3LCJleHAiOjE3ODA0ODEzODcsIl9lbWJlZGRpbmdfcGFyYW1zIjp7ImVuaGV0c25hbW4iOiJlbmFibGVkIiwibSVDMyVBNW5hZCI6ImVuYWJsZWQiLCIlQzMlQTVyc2t1cnMiOiJlbmFibGVkIn19.WgijQtjFVvZjlj4T9PWWeS_JyRPNBAwyo_7dcsU65hk" with-title="true" with-downloads="false"></metabase-dashboard>
                 """
                 st.components.v1.html(metabase_html_code, height=800, scrolling=True)
                 st.divider()
